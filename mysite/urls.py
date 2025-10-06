45e0467356151aa5501df348ff9db61e69c30d8a
Lizanne Fernandes <lizferns160@gmail.com>
2025-10-06 15:30:14 +0530

    Sync project with GitHub: Add home.html and other updates

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('employees/', include('employees.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
